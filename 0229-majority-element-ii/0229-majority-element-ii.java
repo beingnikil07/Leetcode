d2027340a77e4d92d894f4b73aaa0c032d7c055c
Nikil Kumar <115877620+beingnikil07@users.noreply.github.com>
2026-08-18 06:25:44 +0530

    Time: 3 ms (72.7%), Space: 52.6 MB (53.48%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,14 +1,47 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         ArrayList<Integer> list=new ArrayList<>();
-        HashMap<Integer,Integer> map=new HashMap<>();
-        
-        for(int x:nums){
-            map.put(x,map.getOrDefault(x,0)+1);
-            if(map.get(x)>nums.length/3 && ! list.contains(x)){
-                list.add(x);
+        int count1=0,count2=0;
+        int ans1=0, ans2=0;
+        for(int i=0;i<nums.length;i++){
+            if(count1==0&& ans2!=nums[i]){
+                ans1=nums[i];
+                count1=1;
             }
-        }       
+            else if(ans1==nums[i]){
+                count1++;
+            }
+            else if(count2==0 && ans1!=nums[i]){
+                ans2=nums[i];
+                count2=1;
+            }
+            else if(ans2==nums[i]){
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+
+        count1=0;
+        count2=0;
+        for(int x:nums){
+            if(x==ans1){
+                count1++;
+            }
+            if(x==ans2){
+                count2++;
+            }
+        }
+
+        if(count1>nums.length/3 && !list.contains(ans1)){
+            list.add(ans1);
+        }
+        if(count2>nums.length/3 && !list.contains(ans2)){
+            list.add(ans2);
+        }
+
         return list;
     }
 }
