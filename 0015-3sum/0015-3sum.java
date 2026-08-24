9a75bb401fdb6b98c1c04f47562a3880a9f1f2a2
Nikil Kumar <115877620+beingnikil07@users.noreply.github.com>
2026-08-24 14:46:05 +0530

    Time: 29 ms (93.28%), Space: 59.4 MB (32.11%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -4,7 +4,9 @@ class Solution {
         //sort the array
         Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length; i++) {
+        for (int i = 0; i < nums.length - 2; i++) {
+            if (nums[i] > 0)
+                break;
             // Skip duplicates for first element
             if (i > 0 && nums[i] == nums[i - 1])
                 continue;
@@ -32,7 +34,6 @@ class Solution {
                     right--;
             }
 
-            
         }
         return res;
     }
