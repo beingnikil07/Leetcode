70c6915101f84dd59fc6438c64efe1499b53cb87
Nikil Kumar <115877620+beingnikil07@users.noreply.github.com>
2026-08-29 10:29:13 +0530

    Time: 54 ms (24.38%), Space: 48.4 MB (5.48%) - LeetHub

--- 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
+++ 0438-find-all-anagrams-in-a-string/0438-find-all-anagrams-in-a-string.java
@@ -1,29 +1,71 @@
 class Solution {
-    public List<Integer> findAnagrams(String s, String p) {
 
-        List<Integer> ans = new ArrayList<>();
+    public static boolean compare(HashMap<Character, Integer> pMap,
+                                  HashMap<Character, Integer> sMap) {
 
-        for (int i = 0; i <= s.length() - p.length(); i++) {
+        if (pMap.size() != sMap.size()) {
+            return false;
+        }
 
-            int[] freqP = new int[26];
-            int[] freqS = new int[26];
-
-            // frequency of p
-            for (int j = 0; j < p.length(); j++) {
-                freqP[p.charAt(j) - 'a']++;
-            }
-
-            // frequency of current substring
-            for (int j = i; j < i + p.length(); j++) {
-                freqS[s.charAt(j) - 'a']++;
-            }
-
-            // compare frequencies
-            if (Arrays.equals(freqP, freqS)) {
-                ans.add(i);
+        for (char ch : pMap.keySet()) {
+            if (!pMap.get(ch).equals(sMap.get(ch))) {
+                return false;
             }
         }
 
-        return ans;
+        return true;
+    }
+
+    public List<Integer> findAnagrams(String s, String p) {
+
+        HashMap<Character, Integer> pMap = new HashMap<>();
+        HashMap<Character, Integer> sMap = new HashMap<>();
+
+        List<Integer> result = new ArrayList<>();
+
+        if (p.length() > s.length()) {
+            return result;
+        }
+
+        // Frequency of p
+        for (int i = 0; i < p.length(); i++) {
+            char ch = p.charAt(i);
+            pMap.put(ch, pMap.getOrDefault(ch, 0) + 1);
+        }
+
+        // First window
+        for (int i = 0; i < p.length(); i++) {
+            char ch = s.charAt(i);
+            sMap.put(ch, sMap.getOrDefault(ch, 0) + 1);
+        }
+
+        // Check first window
+        if (compare(pMap, sMap)) {
+            result.add(0);
+        }
+
+        // Sliding window
+        for (int i = p.length(); i < s.length(); i++) {
+
+            // Add new character
+            char newChar = s.charAt(i);
+            sMap.put(newChar, sMap.getOrDefault(newChar, 0) + 1);
+
+            // Remove old character
+            char oldChar = s.charAt(i - p.length());
+
+            if (sMap.get(oldChar) == 1) {
+                sMap.remove(oldChar);
+            } else {
+                sMap.put(oldChar, sMap.get(oldChar) - 1);
+            }
+
+            // Check current window
+            if (compare(pMap, sMap)) {
+                result.add(i - p.length() + 1);
+            }
+        }
+
+        return result;
     }
 }
